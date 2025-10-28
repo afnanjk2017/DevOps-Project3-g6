b261bd715421ebd1c0d9a75edf2381420cd59f20
afnanjk2017 <afnanjk2017@gmail.com>
2025-10-28 11:45:40 +0300

    test

--- infra/modules/db/outputs.tf
+++ infra/modules/db/outputs.tf
@@ -1,10 +1,4 @@
-output "db_server_fqdn" {
-  value = azurerm_mssql_server.sql.fully_qualified_domain_name
-}
 
-output "db_name" {
-  value = azurerm_mssql_database.sqldb.name
-}
 
 output "db_admin" {
   value = var.db_admin
